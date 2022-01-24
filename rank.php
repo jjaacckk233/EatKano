4e6da1d2861392dee2b818c0e194f51c1201fd7a
jjaacckk233 <98305902+jjaacckk233@users.noreply.github.com>
2022-01-24 19:07:09 +0800

    Update rank.php

--- rank.php
+++ rank.php
@@ -2,8 +2,8 @@
 <html>
 
 <head>
-  <title>吃掉小鹿乃-排行榜</title>
-  <meta item="description" content="来看神仙" />
+  <title>km语录-排行榜</title>
+  <meta item="description" content="km：零点不是点" />
   <meta itemprop="image" content="https://www.thac.cc/kano/res/logo.jpg" />
   <meta charset="utf-8" />
   <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
